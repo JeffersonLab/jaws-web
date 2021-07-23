3e3debd518539a8d3c41a156cff30e15b2957581
slominskir <Ryan.Slominski@gmail.com>
2021-07-23 13:54:24 -0400

    experimenting with demos

--- src/main/java/org/jlab/jaws/HelloWorld.java
+++ src/main/java/org/jlab/jaws/HelloWorld.java
@@ -1,7 +1,12 @@
 package org.jlab.jaws;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.sse.OutboundSseEvent;
+import javax.ws.rs.sse.Sse;
+import javax.ws.rs.sse.SseBroadcaster;
+import javax.ws.rs.sse.SseEventSink;
 
 @Path("/hello")
 public class HelloWorld {
