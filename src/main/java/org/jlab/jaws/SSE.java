bc56d15fb5c99469e2ab7632136a407b63e5760b
slominskir <Ryan.Slominski@gmail.com>
2021-07-28 15:32:01 -0400

    Add support for registered-classes

--- src/main/java/org/jlab/jaws/SSE.java
+++ src/main/java/org/jlab/jaws/SSE.java
@@ -6,6 +6,8 @@ import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.io.JsonEncoder;
 import org.apache.avro.specific.SpecificDatumWriter;
 import org.jlab.jaws.entity.RegisteredAlarm;
+import org.jlab.jaws.entity.RegisteredClass;
+import org.jlab.jaws.entity.RegisteredClassKey;
 import org.jlab.jaws.eventsource.EventSourceConfig;
 import org.jlab.jaws.eventsource.EventSourceListener;
 import org.jlab.jaws.eventsource.EventSourceRecord;
@@ -54,38 +56,42 @@ public class SSE {
                 final String servers = "localhost:9094";
                 final String registry = "http://localhost:8081";
 
-                final Properties registrationProps = new Properties();
-
-                final SpecificAvroSerde<RegisteredAlarm> VALUE_SERDE = new SpecificAvroSerde<>();
-
-                registrationProps.put(EventSourceConfig.EVENT_SOURCE_GROUP, "sse-proxy-" + Instant.now().toString() + "-" + Math.random());
-                registrationProps.put(EventSourceConfig.EVENT_SOURCE_TOPIC, "registered-alarms");
-                registrationProps.put(EventSourceConfig.EVENT_SOURCE_BOOTSTRAP_SERVERS, servers);
-                registrationProps.put(EventSourceConfig.EVENT_SOURCE_KEY_DESERIALIZER, "org.apache.kafka.common.serialization.StringDeserializer");
-                registrationProps.put(EventSourceConfig.EVENT_SOURCE_VALUE_DESERIALIZER, VALUE_SERDE.deserializer().getClass().getName());
-
-                // Deserializer specific configs
-                registrationProps.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, registry);
-                registrationProps.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, "true");
+                final Properties registrationProps = getRegistrationProps(servers, registry);
+                final Properties classProps = getClassProps(servers, registry);
 
                 try (
                         EventSourceTable<String, RegisteredAlarm> registrationTable = new EventSourceTable<>(registrationProps);
+                        EventSourceTable<RegisteredClassKey, RegisteredClass> classTable = new EventSourceTable<>(classProps);
                 ) {
 
                     registrationTable.addListener(new EventSourceListener<String, RegisteredAlarm>() {
                         @Override
                         public void initialState(Set<EventSourceRecord<String, RegisteredAlarm>> records) {
-                            sendRecords(sink, records);
+                            sendRegistrationRecords(sink, records);
                         }
 
                         @Override
                         public void changes(List<EventSourceRecord<String, RegisteredAlarm>> records) {
-                            sendRecords(sink, records);
+                            sendRegistrationRecords(sink, records);
+                        }
+
+                    });
+
+                    classTable.addListener(new EventSourceListener<RegisteredClassKey, RegisteredClass>() {
+                        @Override
+                        public void initialState(Set<EventSourceRecord<RegisteredClassKey, RegisteredClass>> records) {
+                            sendClassRecords(sink, records);
+                        }
+
+                        @Override
+                        public void changes(List<EventSourceRecord<RegisteredClassKey, RegisteredClass>> records) {
+                            sendClassRecords(sink, records);
                         }
 
                     });
 
                     registrationTable.start();
+                    classTable.start();
 
                     while (!sink.isClosed()) {
                         sink.send(sse.newEvent("ping", ":"));  // Actively check for connection
@@ -103,7 +109,44 @@ public class SSE {
         });
     }
 
-    private void sendRecords(SseEventSink sink, Collection<EventSourceRecord<String, RegisteredAlarm>> records) {
+    private Properties getRegistrationProps(String servers, String registry) {
+        final Properties props = new Properties();
+
+        final SpecificAvroSerde<RegisteredAlarm> VALUE_SERDE = new SpecificAvroSerde<>();
+
+        props.put(EventSourceConfig.EVENT_SOURCE_GROUP, "web-proxy-registered-" + Instant.now().toString() + "-" + Math.random());
+        props.put(EventSourceConfig.EVENT_SOURCE_TOPIC, "registered-alarms");
+        props.put(EventSourceConfig.EVENT_SOURCE_BOOTSTRAP_SERVERS, servers);
+        props.put(EventSourceConfig.EVENT_SOURCE_KEY_DESERIALIZER, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(EventSourceConfig.EVENT_SOURCE_VALUE_DESERIALIZER, VALUE_SERDE.deserializer().getClass().getName());
+
+        // Deserializer specific configs
+        props.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, registry);
+        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, "true");
+
+        return props;
+    }
+
+    private Properties getClassProps(String servers, String registry) {
+        final Properties props = new Properties();
+
+        final SpecificAvroSerde<RegisteredClassKey> KEY_SERDE = new SpecificAvroSerde<>();
+        final SpecificAvroSerde<RegisteredClass> VALUE_SERDE = new SpecificAvroSerde<>();
+
+        props.put(EventSourceConfig.EVENT_SOURCE_GROUP, "web-proxy-class-" + Instant.now().toString() + "-" + Math.random());
+        props.put(EventSourceConfig.EVENT_SOURCE_TOPIC, "registered-classes");
+        props.put(EventSourceConfig.EVENT_SOURCE_BOOTSTRAP_SERVERS, servers);
+        props.put(EventSourceConfig.EVENT_SOURCE_KEY_DESERIALIZER, KEY_SERDE.deserializer().getClass().getName());
+        props.put(EventSourceConfig.EVENT_SOURCE_VALUE_DESERIALIZER, VALUE_SERDE.deserializer().getClass().getName());
+
+        // Deserializer specific configs
+        props.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, registry);
+        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, "true");
+
+        return props;
+    }
+
+    private void sendRegistrationRecords(SseEventSink sink, Collection<EventSourceRecord<String, RegisteredAlarm>> records) {
         for (EventSourceRecord<String, RegisteredAlarm> record : records) {
             String key = record.getKey();
             RegisteredAlarm value = record.getValue();
@@ -112,7 +155,7 @@ public class SSE {
 
             if (value != null) {
                 try {
-                    SpecificDatumWriter<RegisteredAlarm> writer = new SpecificDatumWriter<RegisteredAlarm>(value.getSchema());
+                    SpecificDatumWriter<RegisteredAlarm> writer = new SpecificDatumWriter<>(value.getSchema());
                     OutputStream out = new ByteArrayOutputStream();
                     JsonEncoder encoder = EncoderFactory.get().jsonEncoder(value.getSchema(), out);
                     writer.write(value, encoder);
@@ -126,6 +169,34 @@ public class SSE {
             String recordString = key + "=" + jsonValue;
 
             sink.send(sse.newEvent("registration", recordString));
+        }
+    }
+
+    private void sendClassRecords(SseEventSink sink, Collection<EventSourceRecord<RegisteredClassKey, RegisteredClass>> records) {
+        for (EventSourceRecord<RegisteredClassKey, RegisteredClass> record : records) {
+            RegisteredClassKey keyEnum = record.getKey();
+            RegisteredClass value = record.getValue();
+
+            String key = keyEnum.getClass$().name();
+
+            String jsonValue = null;
+
+            if (value != null) {
+                try {
+                    SpecificDatumWriter<RegisteredClass> writer = new SpecificDatumWriter<>(value.getSchema());
+                    OutputStream out = new ByteArrayOutputStream();
+                    JsonEncoder encoder = EncoderFactory.get().jsonEncoder(value.getSchema(), out);
+                    writer.write(value, encoder);
+                    encoder.flush();
+                    jsonValue = out.toString();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            String recordString = key + "=" + jsonValue;
+
+            sink.send(sse.newEvent("class", recordString));
         }
     }
 }
