e6aa05e683caaf38d80569929ba5b0b752e5bbbf
ryans <ryans@jlab.org>
2021-07-30 14:52:46 -0400

    Add Openapi docs

--- src/main/java/org/jlab/jaws/REST.java
+++ src/main/java/org/jlab/jaws/REST.java
@@ -1,10 +1,13 @@
 package org.jlab.jaws;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+
 import javax.ws.rs.*;
 
 @Path("/rest")
 public class REST {
     @PUT
+    @Operation(description = "Set registration")
     public void putRegistration()
     {
         System.out.println("PUT received");
