18987289a48dd77b151a4b6200cce9d82fa42484
ryans <ryans@jlab.org>
2021-07-30 16:54:22 -0400

    Basic PUT support

--- src/main/java/org/jlab/jaws/REST.java
+++ src/main/java/org/jlab/jaws/REST.java
@@ -1,7 +1,16 @@
 package org.jlab.jaws;
 
 
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.jlab.jaws.entity.AlarmClass;
+import org.jlab.jaws.entity.RegisteredAlarm;
+import org.jlab.jaws.entity.SimpleProducer;
+
 import javax.ws.rs.*;
+import java.util.Properties;
 
 @Path("/rest")
 public class REST {
@@ -9,5 +18,37 @@ public class REST {
     public void putRegistration()
     {
         System.out.println("PUT received");
+
+        final String servers = "localhost:9094";
+        final String registry = "http://localhost:8081";
+        final String registeredTopic = "registered-alarms";
+
+        String key = "alarm4";
+
+        RegisteredAlarm value = new RegisteredAlarm();
+        value.setClass$(AlarmClass.Base_Class);
+        value.setProducer(new SimpleProducer());
+
+        Properties props = getRegistrationProps(servers, registry);
+
+        try(KafkaProducer<String, RegisteredAlarm> producer = new KafkaProducer<>(props)) {
+            producer.send(new ProducerRecord<>(registeredTopic, key, value));
+        }
+    }
+
+
+    private Properties getRegistrationProps(String servers, String registry) {
+        final Properties props = new Properties();
+
+        final SpecificAvroSerde<RegisteredAlarm> VALUE_SERDE = new SpecificAvroSerde<>();
+
+        props.put("bootstrap.servers", servers);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", VALUE_SERDE.serializer().getClass().getName());
+
+        // Serializer specific configs
+        props.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, registry);
+
+        return props;
     }
 }
