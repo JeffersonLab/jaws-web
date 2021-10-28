83cc57363bd064deaabf151fac2a69e7266d55af
slominskir <Ryan.Slominski@gmail.com>
2021-10-28 11:24:43 -0400

    Wait for a JAWS Schema in registry before starting

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM jboss/wildfly:25.0.0.Final
 ADD ./dockerWarVolume/jaws-admin-gui.war /opt/jboss/wildfly/standalone/deployments/
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "----------------------------------------------"
+echo "Step 1: Waiting for a JAWS Schema in Registry "
+echo "----------------------------------------------"
+url=$SCHEMA_REGISTRY
+echo "waiting on: $url"
+while [ $(curl -s -o /dev/null -w %{http_code} $url/subjects/alarm-overrides-value/versions) -ne 200 ] ; do
+  echo -e $(date) " Kafka Registry listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} $url/subjects/alarm-overrides-value/versions) " (waiting for 200)"
+  sleep 5
+done
+
+# Launch original container CMD
+/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0
