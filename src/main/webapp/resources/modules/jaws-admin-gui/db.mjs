36a05876d2b0fb0b51b08501895f4e511dc1a816
slominskir <ryan.slominski@gmail.com>
2022-03-24 15:54:05 -0400

    Version IndexDB

--- src/main/webapp/resources/modules/jaws-admin-gui/db.mjs
+++ src/main/webapp/resources/modules/jaws-admin-gui/db.mjs
@@ -1,9 +1,55 @@
 import Dexie from '../dexie-3.2.1/dexie.min.mjs';
 import {AlarmCategory, AlarmClass, AlarmInstance, AlarmLocation, EffectiveRegistration, KafkaLogPosition} from "./entities.mjs";
 
-const db = new Dexie("jaws");
+const cacheVersion = '@CACHE_VERSION@';
 
-db.version(1).stores({
+// https://github.com/dexie/Dexie.js/issues/349#issuecomment-789173564
+class CacheDB extends Dexie {
+    constructor(name, cacheVersion) {
+        super(name);
+
+        this.cacheVersion = cacheVersion;
+    }
+
+    open() {
+        if (this.isOpen()) return super.open();
+
+        return Dexie.Promise.resolve()
+            .then(() => Dexie.exists(this.name))
+            .then((exists) => {
+                if (!exists) {
+                    // no need to check database version since it doesn't exist
+                    console.log(`IndexDB: Schema doesn't exist, creating new`);
+                    return;
+                }
+
+                // Open separate instance of dexie to get current database vers
+                return new Dexie(this.name).open()
+                    .then(async db => {
+                        if (db.verno == this.cacheVersion) {
+                            console.log(`IndexDB: Schema already up-to-date with version: ${this.cacheVersion}`);
+                            // database up to date (or newer)
+                            return db.close();
+                        }
+
+                        console.log(`IndexDB: Schema out of date, resetting all data. (currentVersion: ${db.verno}, expectedVersion: ${this.cacheVersion})`);
+                        await db.close();
+                        await db.delete();
+
+                        // ensure delete was successful
+                        const exists = await Dexie.exists(this.name);
+                        if (exists) {
+                            throw new Error('IndexDB: Failed to remove mock backend database.');
+                        }
+                    })
+            })
+            .then(() => super.open());
+    }
+}
+
+const db = new CacheDB("jaws", cacheVersion);
+
+db.version(cacheVersion).stores({
     categories: "name",
     classes: "name",
     effectives: "name",
