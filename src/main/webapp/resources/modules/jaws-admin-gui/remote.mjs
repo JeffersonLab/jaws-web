447d401735518bdc678850b302e8fc0a8d5af322
slominskir <ryan.slominski@gmail.com>
2022-05-26 16:36:38 -0400

    Consolidate duplicate code

--- src/main/webapp/resources/modules/jaws-admin-gui/remote.mjs
+++ src/main/webapp/resources/modules/jaws-admin-gui/remote.mjs
@@ -7,7 +7,7 @@ class Remote extends EventTarget {
 
     }
 
-    setInstance(formData) {
+    setEntity(formData, path) {
         /*Treat empty string as no-field*/
         for (var pair of Array.from(formData.entries())) {
             if (pair[1] === "") {
@@ -15,7 +15,7 @@ class Remote extends EventTarget {
             }
         }
 
-        return fetch(contextPath + "/proxy/rest/instance", {
+        return fetch(contextPath + path, {
             method: "PUT",
             body: new URLSearchParams(formData),
             headers: {
@@ -23,32 +23,14 @@ class Remote extends EventTarget {
                 "Content-type": 'application/x-www-form-urlencoded;charset=UTF-8'
             }
         });
+    }
+
+    setInstance(formData) {
+        return this.setEntity(formData, "/proxy/rest/instance");
     }
 
     setClass(formData) {
-        /*Treat empty string as no-field*/
-        for (var pair of Array.from(formData.entries())) {
-            if (pair[1] === "") {
-                formData.delete(pair[0]);
-            }
-        }
-
-        return fetch(contextPath + "/proxy/rest/class", {
-            method: "PUT",
-            body: new URLSearchParams(formData),
-            headers: {
-                Accept: "application/json",
-                "Content-type": 'application/x-www-form-urlencoded;charset=UTF-8'
-            }
-        });
-    }
-
-    getLocations() {
-        return fetch(contextPath + '/proxy/rest/locations')
-    }
-
-    getCategories() {
-        return fetch(contextPath + '/proxy/rest/categories');
+        return this.setEntity(formData, "/proxy/rest/class");
     }
 
     getPriorities() {
@@ -85,52 +67,7 @@ if( supportsWorkerType() ) {
 }
 
 worker.onmessage = function(e) {
-    let event;
-
-    switch(e.data) {
-        case "category":
-            event = new CustomEvent("category", { detail: null });
-            remote.dispatchEvent(event);
-            break;
-        case "category-highwatermark":
-            event = new CustomEvent("category-highwatermark", { detail: null });
-            remote.dispatchEvent(event);
-            break;
-        case "class":
-             event = new CustomEvent("class", { detail: null });
-            remote.dispatchEvent(event);
-            break;
-        case "class-highwatermark":
-            event = new CustomEvent("class-highwatermark", { detail: null });
-            remote.dispatchEvent(event);
-            break;
-        case "instance":
-            event = new CustomEvent("instance", { detail: null });
-            remote.dispatchEvent(event);
-            break;
-        case "instance-highwatermark":
-            event = new CustomEvent("instance-highwatermark", { detail: null });
-            remote.dispatchEvent(event);
-            break;
-        case "location":
-            event = new CustomEvent("location", { detail: null });
-            remote.dispatchEvent(event);
-            break;
-        case "location-highwatermark":
-            event = new CustomEvent("location-highwatermark", { detail: null });
-            remote.dispatchEvent(event);
-            break;
-        case "effective":
-            event = new CustomEvent("effective", { detail: null });
-            remote.dispatchEvent(event);
-            break;
-        case "effective-highwatermark":
-            event = new CustomEvent("effective-highwatermark", { detail: null });
-            remote.dispatchEvent(event);
-            break;
-        default:
-            console.log('Unknown worker message: ', e.data);
-    }
+    remote.dispatchEvent(new CustomEvent(e.data, { detail: null }));
 }
 
 export default remote;
