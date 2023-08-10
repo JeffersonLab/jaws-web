ca320c991ac70ff6a16fb961783e313c6582c17d
slominskir <ryan.slominski@gmail.com>
2023-08-10 15:42:09 -0400

    Wait for registry avoid error [no ci]

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-echo "----------------------------------------------"
-echo "Step 1: Waiting for a JAWS Schema in Registry "
-echo "----------------------------------------------"
-url=$SCHEMA_REGISTRY
-echo "waiting on: $url"
-while [ $(curl -s -o /dev/null -w %{http_code} $url/subjects/alarm-overrides-value/versions) -ne 200 ] ; do
-  echo -e $(date) " Kafka Registry listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} $url/subjects/alarm-overrides-value/versions) " (waiting for 200)"
+echo "-------------------------------------------------------"
+echo "Step 1: Waiting for Schema Registry to start listening "
+echo "-------------------------------------------------------"
+while [ $(curl -s -o /dev/null -w %{http_code} $SCHEMA_REGISTRY/schemas/types) -eq 000 ] ; do
+  echo -e $(date) " Registry listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} $SCHEMA_REGISTRY/schemas/types) " (waiting for 200)"
   sleep 5
 done
 
