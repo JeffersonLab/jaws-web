33b0893824adfe8aea0f11cd49e67d0a1a6d7777
slominskir <ryan.slominski@gmail.com>
2023-12-06 16:58:31 -0500

    filterable [no ci]

--- src/main/java/org/jlab/jaws/business/session/ComponentFacade.java
+++ src/main/java/org/jlab/jaws/business/session/ComponentFacade.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -32,14 +33,28 @@ public class ComponentFacade extends AbstractFacade<Component> {
         super(Component.class);
     }
 
+    private List<Predicate> getFilters(CriteriaBuilder cb, Root<Component> root, String componentName, BigInteger teamId) {
+        List<Predicate> filters = new ArrayList<>();
+
+        if (componentName != null && !componentName.isEmpty()) {
+            filters.add(cb.like(cb.lower(root.get("name")), componentName.toLowerCase()));
+        }
+
+        if (teamId != null) {
+            filters.add(cb.equal(root.get("team"), teamId));
+        }
+
+        return filters;
+    }
+
     @PermitAll
-    public List<Component> filterList(int offset, int max) {
+    public List<Component> filterList(String componentName, BigInteger teamId, int offset, int max) {
         CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
         CriteriaQuery<Component> cq = cb.createQuery(Component.class);
         Root<Component> root = cq.from(Component.class);
         cq.select(root);
         
-        List<Predicate> filters = new ArrayList<>();
+        List<Predicate> filters = getFilters(cb, root, componentName, teamId);
 
         if (!filters.isEmpty()) {
             cq.where(cb.and(filters.toArray(new Predicate[]{})));
@@ -54,13 +69,13 @@ public class ComponentFacade extends AbstractFacade<Component> {
     }
 
     @PermitAll
-    public long countList() {
+    public long countList(String componentName, BigInteger teamId) {
         CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
         CriteriaQuery<Long> cq = cb.createQuery(Long.class);
         Root<Component> root = cq.from(Component.class);
-        
-        List<Predicate> filters = new ArrayList<>();
-        
+
+        List<Predicate> filters = getFilters(cb, root, componentName, teamId);
+
         if (!filters.isEmpty()) {
             cq.where(cb.and(filters.toArray(new Predicate[]{})));
         }
--- src/main/java/org/jlab/jaws/controller/inventory/ComponentController.java
+++ src/main/java/org/jlab/jaws/controller/inventory/ComponentController.java
@@ -2,7 +2,12 @@ package org.jlab.jaws.controller.inventory;
 
 import org.jlab.jaws.business.session.AbstractFacade;
 import org.jlab.jaws.business.session.ComponentFacade;
+import org.jlab.jaws.business.session.TeamFacade;
 import org.jlab.jaws.persistence.entity.Component;
+import org.jlab.jaws.persistence.entity.Team;
+import org.jlab.smoothness.presentation.util.Paginator;
+import org.jlab.smoothness.presentation.util.ParamConverter;
+import org.jlab.smoothness.presentation.util.ParamUtil;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -11,6 +16,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,6 +30,9 @@ public class ComponentController extends HttpServlet {
 
     @EJB
     ComponentFacade componentFacade;
+
+    @EJB
+    TeamFacade teamFacade;
 
     /**
      * Handles the HTTP
@@ -36,10 +47,68 @@ public class ComponentController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        List<Component> componentList = componentFacade.findAll(new AbstractFacade.OrderDirective("name"));
+        String componentName = request.getParameter("componentName");
+        BigInteger teamId = ParamConverter.convertBigInteger(request, "teamId");
+        int offset = ParamUtil.convertAndValidateNonNegativeInt(request, "offset", 0);
+        int maxPerPage = 100;
 
+        List<Component> componentList = componentFacade.filterList(componentName, teamId, offset, maxPerPage);
+        List<Team> teamList = teamFacade.findAll(new AbstractFacade.OrderDirective("name"));
+
+        Team selectedTeam = null;
+
+        if(teamId != null) {
+            selectedTeam = teamFacade.find(teamId);
+        }
+
+        long totalRecords = componentFacade.countList(componentName, teamId);
+
+        Paginator paginator = new Paginator(totalRecords, offset, maxPerPage);
+
+        String selectionMessage = createSelectionMessage(paginator, selectedTeam, componentName);
+
+        request.setAttribute("selectionMessage", selectionMessage);
         request.setAttribute("componentList", componentList);
+        request.setAttribute("teamList", teamList);
+        request.setAttribute("paginator", paginator);
 
         request.getRequestDispatcher("/WEB-INF/views/inventory/components.jsp").forward(request, response);
+    }
+
+    private String createSelectionMessage(Paginator paginator, Team team, String componentName) {
+        DecimalFormat formatter = new DecimalFormat("###,###");
+
+        String selectionMessage = "All Components ";
+
+        List<String> filters = new ArrayList<>();
+
+        if(team != null) {
+            filters.add("Team \"" + team.getName() + "\"");
+        }
+
+        if(componentName != null && !componentName.isBlank()) {
+            filters.add("Component Name \"" + componentName + "\"");
+        }
+
+        if (!filters.isEmpty()) {
+            selectionMessage = filters.get(0);
+
+            for (int i = 1; i < filters.size(); i++) {
+                String filter = filters.get(i);
+                selectionMessage += " and " + filter;
+            }
+        }
+
+        if (paginator.getTotalRecords() < paginator.getMaxPerPage() && paginator.getOffset() == 0) {
+            selectionMessage = selectionMessage + " {" + formatter.format(
+                    paginator.getTotalRecords()) + "}";
+        } else {
+            selectionMessage = selectionMessage + " {"
+                    + formatter.format(paginator.getStartNumber())
+                    + " - " + formatter.format(paginator.getEndNumber())
+                    + " of " + formatter.format(paginator.getTotalRecords()) + "}";
+        }
+
+        return selectionMessage;
     }
 }
