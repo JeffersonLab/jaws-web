2f8c0890b7b04e7997ef0aa5f0b605440b3c4de2
slominskir <ryan.slominski@gmail.com>
2024-05-22 08:44:41 -0400

    populate select overrides on init

--- src/main/java/org/jlab/jaws/business/session/KafkaOverrideFacade.java
+++ src/main/java/org/jlab/jaws/business/session/KafkaOverrideFacade.java
@@ -2,10 +2,10 @@ package org.jlab.jaws.business.session;
 
 import org.jlab.jaws.business.util.KafkaConfig;
 import org.jlab.jaws.clients.OverrideConsumer;
-import org.jlab.jaws.entity.AlarmOverrideKey;
-import org.jlab.jaws.entity.AlarmOverrideUnion;
-import org.jlab.jaws.entity.OverriddenAlarmType;
+import org.jlab.jaws.clients.OverrideProducer;
+import org.jlab.jaws.entity.*;
 import org.jlab.jaws.persistence.entity.Alarm;
+import org.jlab.jaws.persistence.entity.AlarmOverride;
 import org.jlab.kafka.eventsource.EventSourceListener;
 import org.jlab.kafka.eventsource.EventSourceRecord;
 
@@ -15,6 +15,7 @@ import javax.annotation.security.RunAs;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Logger;
@@ -38,11 +39,69 @@ public class KafkaOverrideFacade {
 
     @PostConstruct
     private void init() {
+        populateSelectOverrides();
+
+        overrideFacade.clearCache();
+
         final Properties props = KafkaConfig.getConsumerPropsWithRegistry(-1, false);
         consumer = new OverrideConsumer(props);
         EventSourceListener<AlarmOverrideKey, AlarmOverrideUnion> listener = new OverrideListener<>();
         consumer.addListener(listener);
         consumer.start();
+    }
+
+    private void populateSelectOverrides() {
+        List<OverriddenAlarmType> typeList = new ArrayList<>();
+
+        typeList.add(OverriddenAlarmType.Disabled);
+        typeList.add(OverriddenAlarmType.Filtered);
+        typeList.add(OverriddenAlarmType.Shelved);
+
+        List<AlarmOverride> list = overrideFacade.filterList(typeList, null, null, null, null, null, null, 0, Integer.MAX_VALUE);
+
+        if(list != null && list.size() > 0) {
+            try (OverrideProducer producer = new OverrideProducer(KafkaConfig.getProducerPropsWithRegistry())) {
+                for (AlarmOverride override : list) {
+
+                    String name = override.getOverridePK().getAlarm().getName();
+                    OverriddenAlarmType type = override.getOverridePK().getType();
+
+                    AlarmOverrideKey key = new AlarmOverrideKey(name, type);
+                    AlarmOverrideUnion value = new AlarmOverrideUnion();
+
+                    switch (type) {
+                        case Disabled:
+                            value.setUnion(new DisabledOverride(override.getComments()));
+                            break;
+                        case Filtered:
+                            value.setUnion(new FilteredOverride(override.getComments()));
+                            break;
+                        case Masked:
+                            value.setUnion(new MaskedOverride());
+                            break;
+                        case OnDelayed:
+                            value.setUnion(new OnDelayedOverride());
+                            break;
+                        case OffDelayed:
+                            value.setUnion(new OffDelayedOverride());
+                            break;
+                        case Shelved:
+                            Long expirationLong = override.getExpiration().getTime();
+                            ShelvedReason reason = null;
+                            if(override.getShelvedReason() != null) {
+                                reason = ShelvedReason.valueOf(override.getShelvedReason());
+                            }
+                            value.setUnion(new ShelvedOverride(override.isOneshot(), expirationLong, reason, override.getComments()));
+                            break;
+                        case Latched:
+                            value.setUnion(new LatchedOverride());
+                            break;
+                    }
+
+                    producer.send(key, value);
+                }
+            }
+        }
     }
 
     @PreDestroy
--- src/main/java/org/jlab/jaws/business/session/OverrideFacade.java
+++ src/main/java/org/jlab/jaws/business/session/OverrideFacade.java
@@ -3,17 +3,20 @@ package org.jlab.jaws.business.session;
 import org.jlab.jaws.business.util.KafkaConfig;
 import org.jlab.jaws.clients.OverrideProducer;
 import org.jlab.jaws.entity.*;
-import org.jlab.jaws.persistence.entity.Alarm;
-import org.jlab.jaws.persistence.entity.AlarmOverride;
-import org.jlab.jaws.persistence.entity.OverridePK;
+import org.jlab.jaws.persistence.entity.*;
+import org.jlab.jaws.persistence.model.BinaryState;
 import org.jlab.smoothness.business.exception.UserFriendlyException;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.Date;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.math.BigInteger;
+import java.util.*;
 import java.util.logging.Logger;
 
 /**
@@ -26,6 +29,9 @@ public class OverrideFacade extends AbstractFacade<AlarmOverride> {
 
     @PersistenceContext(unitName = "webappPU")
     private EntityManager em;
+
+    @EJB
+    LocationFacade locationFacade;
 
     @Override
     protected EntityManager getEntityManager() {
@@ -84,5 +90,125 @@ public class OverrideFacade extends AbstractFacade<AlarmOverride> {
                 producer.send(key, value);
             }
         }
+    }
+
+    @RolesAllowed("jaws-admin")
+    public void clearCache() {
+        em.createQuery("delete from AlarmOverride").executeUpdate();
+    }
+
+    private List<Predicate> getFilters(CriteriaBuilder cb, CriteriaQuery<? extends Object> cq, Root<AlarmOverride> root,
+                                       List<OverriddenAlarmType> typeList,
+                                       BigInteger[] locationIdArray,
+                                       BigInteger priorityId, BigInteger teamId, String alarmName, String actionName,
+                                       String componentName, Map<String, Join> joins) {
+        List<Predicate> filters = new ArrayList<>();
+
+        Join<AlarmOverride, Alarm> alarmJoin = root.join("overridePK").join("alarm");
+        Join<Alarm, Action> actionJoin = alarmJoin.join("action");
+        Join<Action, Component> componentJoin = actionJoin.join("component");
+
+        joins.put("alarm", alarmJoin);
+        joins.put("action", actionJoin);
+        joins.put("component", componentJoin);
+
+        if(typeList != null && typeList.size() > 0) {
+            filters.add(root.get("overridePK").get("type").in(typeList));
+        }
+
+        if(locationIdArray != null && locationIdArray.length > 0) {
+            // Parent locations imply children locations
+            Set<Location> materializedLocations = new HashSet<>();
+            for(BigInteger locationId: locationIdArray) {
+                if(locationId != null) {
+                    Set<Location> subset = locationFacade.findBranchAsSet(locationId);
+                    materializedLocations.addAll(subset);
+                }
+            }
+
+            List<BigInteger> locationIdList = new ArrayList<>();
+
+            for(Location l: materializedLocations) {
+                locationIdList.add(l.getLocationId());
+            }
+
+            if(!locationIdList.isEmpty()) {
+                Subquery<BigInteger> subquery = cq.subquery(BigInteger.class);
+                Root<Location> subqueryRoot = subquery.from(Location.class);
+                Join<Location, Alarm> alarmLocationJoin = subqueryRoot.join("alarmList");
+                subquery.select(alarmLocationJoin.get("alarmId"));
+                subquery.where(subqueryRoot.get("locationId").in(locationIdList));
+                filters.add(cb.in(alarmJoin.get("alarmId")).value(subquery));
+            }
+        }
+
+        if (alarmName != null && !alarmName.isEmpty()) {
+            filters.add(cb.like(cb.lower(alarmJoin.get("name")), alarmName.toLowerCase()));
+        }
+
+        if (priorityId != null) {
+            filters.add(cb.equal(actionJoin.get("priority"), priorityId));
+        }
+
+        if (actionName != null && !actionName.isEmpty()) {
+            filters.add(cb.like(cb.lower(actionJoin.get("name")), actionName.toLowerCase()));
+        }
+
+        if (componentName != null && !componentName.isEmpty()) {
+            filters.add(cb.like(cb.lower(componentJoin.get("name")), componentName.toLowerCase()));
+        }
+
+        if (teamId != null) {
+            filters.add(cb.equal(componentJoin.get("team"), teamId));
+        }
+
+        return filters;
+    }
+
+    @PermitAll
+    public List<AlarmOverride> filterList(List<OverriddenAlarmType> typeList, BigInteger[] locationIdArray, BigInteger priorityId, BigInteger teamId, String alarmName, String actionName, String componentName, int offset, int max) {
+        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<AlarmOverride> cq = cb.createQuery(AlarmOverride.class);
+        Root<AlarmOverride> root = cq.from(AlarmOverride.class);
+        cq.select(root);
+
+        Map<String, Join> joins = new HashMap<>();
+
+        List<Predicate> filters = getFilters(cb, cq, root, typeList, locationIdArray, priorityId, teamId, alarmName, actionName,
+                componentName, joins);
+
+        if (!filters.isEmpty()) {
+            cq.where(cb.and(filters.toArray(new Predicate[]{})));
+        }
+
+        List<Order> orders = new ArrayList<>();
+        Path p0 = joins.get("action").get("priority");
+        Order o0 = cb.asc(p0);
+        orders.add(o0);
+        Path p1 = joins.get("alarm").get("name");
+        Order o1 = cb.asc(p1);
+        orders.add(o1);
+        cq.orderBy(orders);
+        return getEntityManager().createQuery(cq).setFirstResult(offset).setMaxResults(max).getResultList();
+    }
+
+    @PermitAll
+    public long countList(List<OverriddenAlarmType> typeList, BigInteger[] locationIdArray, BigInteger priorityId, BigInteger teamId, String alarmName, String actionName, String componentName) {
+        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+        Root<AlarmOverride> root = cq.from(AlarmOverride.class);
+
+        Map<String, Join> joins = new HashMap<>();
+
+        List<Predicate> filters = getFilters(cb, cq, root, typeList, locationIdArray, priorityId, teamId, alarmName, actionName,
+                componentName, joins);
+
+        if (!filters.isEmpty()) {
+            cq.where(cb.and(filters.toArray(new Predicate[]{})));
+        }
+
+        cq.select(cb.count(root));
+        TypedQuery<Long> q = getEntityManager().createQuery(cq);
+        return q.getSingleResult();
     }
 }
